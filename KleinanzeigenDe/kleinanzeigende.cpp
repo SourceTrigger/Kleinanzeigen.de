22986b9ef6fc011e72ca1c74f4b16e5a78f50d13
SourceTrigger <sourcetrigger@gmail.com>
2018-05-12 11:53:50 +0200

    Initial

---
+++ KleinanzeigenDe/kleinanzeigende.cpp
@@ -0,0 +1,6 @@
+#include "kleinanzeigende.h"
+
+
+KleinanzeigenDe::KleinanzeigenDe()
+{
+}
---
+++ KleinanzeigenDe/kleinanzeigende.h
@@ -0,0 +1,13 @@
+#ifndef KLEINANZEIGENDE_H
+#define KLEINANZEIGENDE_H
+
+#include "kleinanzeigende_global.h"
+
+class KLEINANZEIGENDESHARED_EXPORT KleinanzeigenDe
+{
+
+public:
+    KleinanzeigenDe();
+};
+
+#endif // KLEINANZEIGENDE_H
