bf18cc96fb4cd84d4b14711c34ad74b69d3f6662
SourceTrigger <sourcetrigger@gmail.com>
2018-05-12 20:21:25 +0200

    HTTP und String Funktionen hinzufügt inkl. Test

--- KleinanzeigenDe/kleinanzeigende.cpp
+++ KleinanzeigenDe/kleinanzeigende.cpp
@@ -1,5 +1,7 @@
 #include "kleinanzeigende.h"
 #include <QDebug>
+#include <QNetworkReply>
+#include <QNetworkAccessManager>
 
 KleinanzeigenDe::KleinanzeigenDe(QObject *parent)
 {
@@ -9,7 +11,24 @@ KleinanzeigenDe::KleinanzeigenDe(QObject *parent)
 
 QList<SearchResult> KleinanzeigenDe::Search(const QUrl &url, int readpages)
 {
-    qInfo() << "IS TRIGGERED";
+    bool doNext = true;
+    int currentpage = 1;
+    QUrl nextUrl = url;
+
+    while(doNext)
+    {
+        doNext = false;
+
+        QUrlQuery noneQuery;
+        QString htmlSource = GetHtmlSourceCode(nextUrl.toString(), noneQuery);
+
+
+
+        currentpage++;
+        if (currentpage>readpages)
+            break;
+    }
+
     SearchResult result;
     result.AdDescription = "Hallo Welt";
     result.AdId = "12345677";
@@ -51,4 +70,95 @@ QString KleinanzeigenDe::GetPlatformLetters()
 bool KleinanzeigenDe::SendQuestionToAdOwner(const QString &accountUsername, const QString &accountPassword, const QString &myName, const QString &myPhone, const QString &advertId)
 {
     return false; // not implemented
+}
+
+
+
+/**
+ * @brief KleinanzeigenDe::GetHtmlSourceCode
+ * @param rUrl Source URL
+ * @param rPost Any Postfields if needed, else use GET
+ * @return HTML Source Code
+ */
+QString KleinanzeigenDe::GetHtmlSourceCode(const QString &url, const QUrlQuery &post)
+{
+    QNetworkAccessManager manager;
+
+    QUrl uri(url);
+    QEventLoop eventLoop;
+
+    QNetworkRequest request(uri);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
+    request.setHeader(QNetworkRequest::UserAgentHeader, userAgent);
+
+    QObject::connect(&manager, SIGNAL(finished(QNetworkReply*)), &eventLoop, SLOT(quit()));
+    QNetworkReply *reply;
+    if (post.isEmpty())
+        reply = manager.get(request);
+    else
+        reply = manager.post(request,post.toString(QUrl::FullyEncoded).toUtf8());
+    eventLoop.exec();
+
+    QString responseString = "";
+    auto error_type = reply->error();
+    if (error_type == QNetworkReply::NoError) {
+        responseString = reply->readAll();
+    } else {
+        lastError = reply->errorString();
+    }
+
+    reply->deleteLater();
+    manager.deleteLater();
+
+    return responseString;
+}
+
+/**
+ * @brief KleinanzeigenDe::GetPartOfString
+ * @param rSourceString String to parse
+ * @param rFromString Begin of part
+ * @param rToString End of part
+ * @return SubString of begin and end, return empty String if not found
+ */
+QString KleinanzeigenDe::GetPartOfString(const QString &sourceString, const QString &fromString, const QString &toString)
+{
+    QString src = sourceString;
+
+    if (src.isEmpty())
+        return "";
+
+    int start = 0;
+    if (fromString != "")
+    {
+        start = src.indexOf(fromString);
+        if (start >= 0)
+        {
+            start += fromString.length();
+            int end = src.length();
+            if (toString != "")
+            {
+                end = src.indexOf(toString, start);
+                if (end < 0) return "";
+            }
+            return src.mid(start, end - start);
+        }
+        else
+        {
+            return "";
+        }
+    }
+    else
+    {
+        int end = src.length();
+        if (toString != "")
+        {
+            end = src.indexOf(toString, start + fromString.length());
+            if (end < 0) return "";
+        }
+        if (end - start < 0)
+        {
+            return "";
+        }
+        return src.mid(start, end - start);
+    }
 }
--- KleinanzeigenDe/kleinanzeigende.h
+++ KleinanzeigenDe/kleinanzeigende.h
@@ -11,7 +11,10 @@ class KleinanzeigenDe : public QObject, public SearchAgentInterface
         Q_INTERFACES(SearchAgentInterface)
     Q_PLUGIN_METADATA(IID "MG.AnzeigenChef.SearchAgentInterface" FILE "KleinanzeigenDe.json")
 private:
+    QString userAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.20 (KHTML, like Gecko) Chrome/11.0.672.2 Safari/534.20";
     QString lastError;
+    QString GetHtmlSourceCode(const QString &url, const QUrlQuery &post);
+    QString GetPartOfString(const QString &sourceString, const QString &fromString, const QString &toString);
 
 public:
     KleinanzeigenDe(QObject *parent = 0);
